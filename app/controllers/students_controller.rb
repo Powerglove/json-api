82c32867a8a0146c743d9d02d30c13bdc4e1e83b
Powerglove <lang.joh.jl@gmail.com>
2016-07-12 14:20:06 +0200

    First Commit

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,50 @@
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all 
+    render json: @students
+  end
+
+  def show
+    set_student
+    render json: @student
+  end
+
+  def create
+    @student = Student.new(student_params)
+
+    if @student.save
+      render json: @student, status: :created, location: @student
+    else
+      render json: @student.errors, status: :unprocessable_entity
+    end
+    
+  end
+
+  def update
+    @student = Student.find(params[:id])
+
+    if @student.update(student_params)
+      head :no_content
+    else
+      render json: @student.errors, status: :unprocessable_entity
+    end 
+  end
+
+  def destroy
+    @student.destroy
+
+    head :no_content
+  end
+
+  private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:user).permit(:name, :email)
+  end
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+Student.create(name: 'Dayana', email: 'dayana@example.com')
+Student.create(name: 'Johanna', email: 'johanna@example.com')
+
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
