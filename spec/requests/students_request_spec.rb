5e3939604ace7e4111263c226ddc413c99695090
Powerglove <lang.joh.jl@gmail.com>
2016-07-14 16:36:21 +0200

    correct show spec, write create spec

--- spec/requests/students_request_spec.rb
+++ spec/requests/students_request_spec.rb
@@ -1,17 +1,48 @@
-require 'spec_helper'
 require 'rails_helper'
 
 
-describe "Students", type: :request do
-  describe 'GET /students/1' do
-    it "shows the student with the id" do
-       let(:student)
-      FactoryGirl.create(:student)
-      get "/students/#{}"
+RSpec.describe StudentsController, type: :request do
+  
+  context 'with student present' do
+  let(:student) {  FactoryGirl.create :student }
+
+  #def student
+  #  @student ||= FactoryGirl.create(:student)
+  #end
+
+  describe "GET /students/:id" do
+    it "finds student by id" do
+      
+      get "/students/#{student.id}.json"
       expect(response.status).to eq(200)
     end
 
+    it "renders JSON by default" do
+      get "/students/#{student.id}"
+      expected = ActiveSupport::JSON.decode(student.to_json)
+      parsed_response = ActiveSupport::JSON.decode(response.body)
+      expect(parsed_response).to eql expected
+    end
   end
+  end
+
+  describe "POST 'create'" do
+    let(:student_params) { FactoryGirl.attributes_for(:student) }
+
+    it "returns correct status code" do 
+      post "/students", { student: student_params }
+      expect(assigns[:student]).to be_present # checks if the controller has an instance variable named @student assigned but we do not do this in request spesc
+      # because instance vars are "implementation details" not relevant for request "result"
+      expect(response.status).to eq(201)
+    end
+
+    it "creates new student" do
+      expect { post "/students", { student: student_params }  }.to change{ Student.count }.by(1)
+    end
+  end
+
+
+
 end
 
 
